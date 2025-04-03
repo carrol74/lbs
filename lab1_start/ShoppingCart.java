7d0157f1fc1df206cc81079c7c0f236b5b53b772
yushuh <yushuh@git.chalmers.se>
2025-04-03 14:36:30 +0200

    basic program

---
+++ lab1_start/ShoppingCart.java
@@ -0,0 +1,39 @@
+import backEnd.*;
+import java.util.Scanner;
+
+public class ShoppingCart {
+    private static void print(Wallet wallet, Pocket pocket) throws Exception {
+        System.out.println("Your current balance is: " + wallet.getBalance() + " credits.");
+        System.out.println(Store.asString());
+        System.out.println("Your current pocket is:\n" + pocket.getPocket());
+    }
+
+    private static String scan(Scanner scanner) throws Exception {
+        System.out.print("What do you want to buy? (type quit to stop) ");
+        return scanner.nextLine();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Wallet wallet = new Wallet();
+        Pocket pocket = new Pocket();
+        Scanner scanner = new Scanner(System.in);
+
+        print(wallet, pocket);
+        String product = scan(scanner);
+
+        while(!product.equals("quit")) {
+            int price = Store.getProductPrice(product);
+            int balance = wallet.getBalance();
+            if (balance < price) {
+                System.exit(0);
+            } else {
+                wallet.setBalance(balance - price);
+                pocket.addProduct(product);
+            }
+
+            // Just to print everything again...
+            print(wallet, pocket);
+            product = scan(scanner);
+        }
+    }
+}
